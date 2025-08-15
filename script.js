95c9537ac1d82d22d4c637a970e4ca855a72af10
Kleber Perszel Megias Milani <138407889+MestreKleber@users.noreply.github.com>
2025-08-15 10:21:00 -0300

    muitofoda

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+botaoPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector('#entrada-de-texto').value;
-    const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavrasChave = processaTexto(texto);
+    const text = document.querySelector('#entrada-de-texto').value;
+    const resultado = document.querySelector('#resultado-palavrachave');
+    const keyword = processaTexto(texto);
 
-    campoResultado.textContent = palavrasChave.join(", ");
+    resultado.textContent = keyword.join(", ");
 }
 
-function processaTexto(texto) {
-    let palavras = texto.split(/\P{L}+/u);
+function processaTexto(text) {
+    let palavras = text.split(/\P{L}+/u);
     return palavras;
 }
